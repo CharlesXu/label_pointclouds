9c42e056f8a6bdab25aaf37c3d5e19f8d04e4c43
Shankar, Kumar Shaurya <kumarsha@cs.cmu.edu>
2014-11-12 16:46:08 -0500

    Added the correct update routine for the SVM Loss function evaluation needs to be smarter, therefore commented out estimating ||f||^2

--- src/OnlineKernelSVM.py
+++ src/OnlineKernelSVM.py
@@ -6,6 +6,8 @@ An implementation of Kernel SVM with training using online gradient descent
 import numpy as np
 from scipy.linalg.basic import inv
 import pdb
+from LogReader import LogReader
+import pickle
 
 class OKSVM:
     
@@ -18,30 +20,41 @@ class OKSVM:
         self.alphas = np.zeros(1)
         self.learning_rate = params[0]
         self.kernel_type = params[1]
+        self.regularizer = params[2] 
         
     def train(self):
-
         for data_pt in range(self.trainX.shape[0]):
 #             print 'alphas', self.alphas
             prediction = self.eval_function(self.alphas, self.supportVecs, self.trainX[data_pt])
 #             print 'kernelResult' , kernelResult
 #             print 'label', self.trainY[data_pt]
             err =  self.eval_loss(prediction, self.trainY[data_pt])
-            print 'err', err
+#             print 'err', err
+            print data_pt
             if err > 0:
 #                 self.supportVecs = np.concatenate((self.supportVecs.T, np.array([self.trainX[data_pt]]))).T
                 
                 #Add current data point to the 'supportVecs'
                 self.supportVecs = np.vstack( (self.supportVecs, self.trainX[data_pt]) )
 #                 print 'alphas', self.alphas
-                gradient = np.array( [ -self.trainY[data_pt] ])
+                '''
+                update = {  add kernel wighted by \eta y_t if correct
+                         {  shrink all other weights by 1 - 2 \eta \lambda  
+                '''
+                self.alphas = self.alphas * (1.0 - 2*self.learning_rate*self.regularizer)
 #                 print 'supportVecs', self.supportVecs
-                self.alphas = np.append(self.alphas, -self.learning_rate * gradient)
-
+                self.alphas = np.append(self.alphas, self.learning_rate*self.trainY[data_pt])
 
 
     def eval_loss(self, prediction, label_value):
-        return max(0.0, 1 - (prediction * label_value))
+        '''
+        ||f||^2 = <f, f> = 
+        '''
+        #Commenting this out until we have a smarter way of removing support vectors
+#         K = np.zeros((self.supportVecs.shape[0], self.supportVecs.shape[0]) )
+#         for index,vector in enumerate(self.supportVecs):
+#             K[index,:] = self.linear_kernel(self.supportVecs, vector) 
+        return max(0.0, 1 - (prediction * label_value)) #+ self.regularizer*np.dot(self.alphas.T, np.dot(K, self.alphas)) )
         
     def eval_function(self, alphas, supportvecs, dataX):
 #         s = 0
@@ -90,11 +103,33 @@ if __name__ == "__main__":
     Xs = np.array([[1,1],[-1,-1], [1,-1], [-1,1]])
     Ys = np.array([1, -1, -1, 1] )
 
-    svm_params = [0.4, 'linear']
+    svm_params = [0.4, 'linear', 0.01]
     test_svm = OKSVM(Xs, Ys, svm_params)
     test_svm.train()
     print test_svm.test(np.array([0, 1]) , 1)
     print test_svm.test(np.array([1, -0.1]) , -1)
     print test_svm.test(np.array([0, -1]), -1 )
     print test_svm.test(np.array([-1, 0.1]), 1 )
+    
+    train_log_object = LogReader('../data/oakland_part3_am_rf.node_features')
+    test_log_object = LogReader('../data/oakland_part3_an_rf.node_features')
+    train_points = train_log_object.read()
+    test_points = test_log_object.read()
+    trainXs = np.array([point._feature for point in train_points])
+    trainYs = np.array([point._label for point in train_points])
+    testXs = np.array([point._feature for point in test_points])
+    testYs = np.array([point._label for point in test_points])
+    test_svm = OKSVM(trainXs, trainYs, svm_params)
+    test_svm.train()
+    
+    evals = []
+    for index in range(len(testXs)):
+        dataX = testXs[index]
+        true_label = testYs[index]
+        classifier_index = test_svm.predict(dataX)
+        evals.append(test_svm.test(dataX, true_label))
+    #Now evaluate accuracy
+    #True == 1, thus sum
+    print '[OneVsAll] Accuracy = ', float(sum(evals))/len(evals)
+    print 'done'
     #print test_regression.predict(np.array([0.5, 1])), test_regression.test(np.array([0.5, 1]), 1)
